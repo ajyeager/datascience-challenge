3abfce713979d048d5599f83e86385bf5a17ef3b
Vijay Viswanathan <vcomeinpeace@gmail.com>
2018-08-06 01:55:55 -0700

    group data by label

--- load_data.py
+++ load_data.py
@@ -1,6 +1,6 @@
 import json
 from collections import defaultdict
-import sklearn
+from sklearn import model_selection
 
 data_dir="/Users/vijay/Documents/code/yelp-dataset"
 reviews_file = data_dir + "/yelp_academic_dataset_review.json"
@@ -12,11 +12,20 @@ for row in reviews_raw:
     review = json.loads(row)
     reviews.append(review)
 
+train, test = train_test_split(reviews)
+
+reviews_by_rating = reviews_by_star(train)
+
+# TODO(Vijay): add ML models to predict the rating from each movie
 
 def train_test_split(reviews):
-    sklearn.model_selection.train_test_split(reviews)
+    [train, test] = model_selection.train_test_split(reviews, test_size=0.25)
+    return train, test
 
-for review in reviews:
-    rating = review['stars']
-    text = review['text']
+def reviews_by_star(train):
     reviews_by_star = defaultdict(list)
+    for review in train:
+        rating = review['stars']
+        text = review['text']
+        reviews_by_star[rating].append(text)
+    return reviews_by_star
